f70889f5b0d440e879b9da5f33064c2f8eb20780
Alan Boglioli <alan.boglioli@gmail.com>
2019-03-07 21:48:14 -0300

    remove utils

--- test/kompute.test.js
+++ test/kompute.test.js
@@ -3,7 +3,7 @@ const Kompute = require('../lib/kompute');
 
 const { getSimple } = require('./data');
 
-describe('Kompute', () => {
+describe('Kompute class', () => {
   test('Pass array as first argument', () => {
     try {
       kompute({ value: 1 });
@@ -78,32 +78,27 @@ describe('Kompute', () => {
       },
     ]);
   });
+});
 
-  test.skip('Return computed array', () => {
+describe('kompute lib', () => {
+  test('default options', () => {
     const simple = getSimple();
     const k = kompute(simple);
     expect(Array.isArray(k)).toBe(true);
     expect(k).toHaveLength(simple.length);
-  });
-
-  test.skip('isObserved property', () => {
-    const arr = kompute(getSimple());
-    arr.forEach(item => {
-      expect(item.isObserved).toBeDefined();
-      expect(item.isObserved).toBe(true);
-    });
-  });
-
-  test.skip('Initial computation', () => {
-    const simple = getSimple();
-    const k = kompute(simple);
-
-    expect(k).not.toEqual(simple);
-
-    expect(k[0]).toEqual({ id: 'element1', value: 2 });
-    expect(k[1]).toEqual({ id: 'element2', value: 4 });
-    expect(k[2]).toEqual({ id: 'element3', value: 6 });
-    expect(k[0].isObserved).toBeDefined();
-    expect(k[0].observedBy).toEqual(['element2.value', 'element3.value']);
+    expect(k).toEqual([
+      {
+        id: 'id01',
+        value: 2,
+      },
+      {
+        id: 'id02',
+        value: 4,
+      },
+      {
+        id: 'id03',
+        value: 6,
+      },
+    ]);
   });
 });
